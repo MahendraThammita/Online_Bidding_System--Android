8902f21c805161d4b91219875b8704f22ae7eaf0
MahendraThammita <dhmmpthammita@gmail.com>
2020-08-14 19:37:20 +0530

    myBidsCreated

---
+++ app/src/main/java/com/example/online_bidding_system/myBids.java
@@ -0,0 +1,14 @@
+package com.example.online_bidding_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myBids extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_bids);
+    }
+}
