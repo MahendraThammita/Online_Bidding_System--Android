700b273f448c920d6f2cc2e506981b7e9a3a046a
MahendraThammita <dhmmpthammita@gmail.com>
2020-08-14 20:19:19 +0530

    gradel bug fixed

--- app/src/main/java/com/example/online_bidding_system/MainActivity.java
+++ app/src/main/java/com/example/online_bidding_system/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.online_bidding_system;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button profBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profBtn = findViewById(R.id.actionBarProfile);
+
+        profBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent profIntent = new Intent(getApplicationContext() , myBids.class);
+                startActivity(profIntent);
+            }
+        });
 
         Toolbar profTop = findViewById(R.id.profActionbar);
         profTop.setTitle("");
--- app/src/main/java/com/example/online_bidding_system/myBids.java
+++ app/src/main/java/com/example/online_bidding_system/myBids.java
@@ -1,6 +1,7 @@
 package com.example.online_bidding_system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -11,4 +12,6 @@ public class myBids extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_bids);
     }
+
+
 }
