a59cfe751863eecdc6285f97aca14120740ccf03
MahendraThammita <dhmmpthammita@gmail.com>
2020-08-15 11:07:23 +0530

    bug in adapter view solved

--- app/src/main/java/com/example/online_bidding_system/myBids.java
+++ app/src/main/java/com/example/online_bidding_system/myBids.java
@@ -3,15 +3,46 @@ package com.example.online_bidding_system;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class myBids extends AppCompatActivity {
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_bids);
+
+
+        String items[] = {"Item Name 1" , "Item Name 2" , "Item Name 3" , "Item Name 4" , "Item Name 5" , "Item Name 6" , "Item Name 7"};
+
+        ArrayAdapter arradpt = new ArrayAdapter<String>(this,R.layout.my_bid_card , R.id.myBidCardTitle, items);
+
+        lv = findViewById(R.id.myBidsList);
+
+        lv.setAdapter(arradpt);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            Intent in = new Intent(myBids.this , main_categories.class);
+            @Override
+            public void onItemClick(AdapterView<?> listView, View view, int i, long l) {
+
+                startActivity(in);
+            }
+       });
     }
+
+//    public void gotoMore(){
+//        Intent in2 = new Intent(this , MainActivity.class);
+//        startActivity(in2);
+//    }
+
 
 
 }
