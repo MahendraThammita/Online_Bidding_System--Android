953bdbab432f7acd8bc5e033a7e481794faac9f4
MahendraThammita <dhmmpthammita@gmail.com>
2020-08-15 14:34:41 +0530

    Bid image swiper added

---
+++ app/src/main/java/com/example/online_bidding_system/HelperClasser/BiddingAdapters/BidSwiperClass.java
@@ -0,0 +1,14 @@
+package com.example.online_bidding_system.HelperClasser.BiddingAdapters;
+
+public class BidSwiperClass {
+
+    int image;
+
+    public BidSwiperClass(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
