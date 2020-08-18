34eaaf7c5ce5812756a5c2d6d3fc6f5b3f546ed4
RaviBx <jeetkunedoRS1234>
2020-08-18 11:05:54 +0530

    Updated LoginPage

--- app/src/main/java/com/example/online_bidding_system/LogIn_Page.java
+++ app/src/main/java/com/example/online_bidding_system/LogIn_Page.java
@@ -2,13 +2,33 @@ package com.example.online_bidding_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn_Page extends AppCompatActivity {
+
+    Button uslogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in__page);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        uslogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent logintent = new Intent(LogIn_Page.this, HomePage.class);
+                startActivity(logintent);
+            }
+        });
     }
 }
