9a2e4078df5da84426152f9196bb48b8c4647969
RaviBx <jeetkunedoRS1234>
2020-08-18 11:14:41 +0530

    Updated DVDMovies category

--- app/src/main/java/com/example/online_bidding_system/DVDnMovies_category.java
+++ app/src/main/java/com/example/online_bidding_system/DVDnMovies_category.java
@@ -3,12 +3,38 @@ package com.example.online_bidding_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DVDnMovies_category extends AppCompatActivity {
+
+    Button pubNow;
+    Button pubLater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_d_v_dn_movies_category);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        pubNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        pubLater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
