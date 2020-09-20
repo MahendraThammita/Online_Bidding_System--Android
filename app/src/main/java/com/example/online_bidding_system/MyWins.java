f447e30a04abec374bca5a193f194799ab52ea4e
MahendraThammita <dhmmpthammita@gmail.com>
2020-09-20 13:06:17 +0530

    My Wins Page edited

--- app/src/main/java/com/example/online_bidding_system/MyWins.java
+++ app/src/main/java/com/example/online_bidding_system/MyWins.java
@@ -1,19 +1,83 @@
 package com.example.online_bidding_system;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MyWins extends AppCompatActivity {
 
     ListView list;
+    DrawerLayout drawer;
+    NavigationView navi;
+    Toolbar primTool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_wins);
+
+
+
+        drawer = findViewById(R.id.DrwerLay);
+        navi = (NavigationView) findViewById(R.id.nav_view);
+        primTool = findViewById(R.id.primaryActbar);
+
+        setSupportActionBar(primTool);
+
+
+        navi.bringToFront();
+        ActionBarDrawerToggle toggle1 = new ActionBarDrawerToggle(this , drawer , primTool , R.string.OpenDrawerDes , R.string.CloseDrawerDes);
+        drawer.addDrawerListener(toggle1);
+        toggle1.syncState();
+
+
+
+        navi.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.Drawable_myBids:
+                        Intent in1 = new Intent(MyWins.this , myBids.class);
+                        break;
+                    case R.id.Drawable_myWins:
+                        Toast.makeText(MyWins.this , "You are in My Wins page" , Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.Drawable_ViewAuctions:
+                        Intent in2 = new Intent(MyWins.this , HomePage.class);
+                        startActivity(in2);
+                        break;
+                    case R.id.Drawable_myAuctions:
+                        Intent in3 = new Intent(getApplicationContext() , MyAuctions.class);
+                        startActivity(in3);
+                        break;
+                    default:
+                        Intent in6 = new Intent(getApplicationContext() , MyAuctions.class);
+                        startActivity(in6);
+                }
+                return true;
+            }
+        });
+
+
+
+
+
+
+
+
+
 
         String items[] = {"Gaucci Watch" , "Item Name 2" , "Item Name 3" , "Item Name 4" , "Item Name 5" , "Item Name 6" , "Item Name 7"};
 
