85b69af679b349ce63072106a53509609feb6605
MahendraThammita <dhmmpthammita@gmail.com>
2020-09-21 20:59:09 +0530

    EditUser page edit compleated

--- app/src/main/java/com/example/online_bidding_system/Edit_User.java
+++ app/src/main/java/com/example/online_bidding_system/Edit_User.java
@@ -1,14 +1,74 @@
 package com.example.online_bidding_system;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Edit_User extends AppCompatActivity {
+
+    RecyclerView.Adapter adapter;
+    DrawerLayout drawer;
+    NavigationView navi;
+    Toolbar primTool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__user);
+
+        drawer = findViewById(R.id.DrwerLay);
+        navi = (NavigationView) findViewById(R.id.nav_view);
+        primTool = findViewById(R.id.primaryActbar);
+
+        setSupportActionBar(primTool);
+
+
+        navi.bringToFront();
+        ActionBarDrawerToggle toggle1 = new ActionBarDrawerToggle(this , drawer , primTool , R.string.OpenDrawerDes , R.string.CloseDrawerDes);
+        drawer.addDrawerListener(toggle1);
+        toggle1.syncState();
+
+
+//        navi.setNavigationItemSelectedListener(this);
+
+
+        navi.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.Drawable_myBids:
+                        Intent in0 = new Intent(Edit_User.this , myBids.class);
+                        startActivity(in0);
+                        break;
+                    case R.id.Drawable_myWins:
+                        Intent in1 = new Intent(Edit_User.this , MyWins.class);
+                        startActivity(in1);
+                        break;
+                    case R.id.Drawable_ViewAuctions:
+                        Intent in2 = new Intent(Edit_User.this ,MyAuctions.class);
+                        startActivity(in2);
+                        break;
+                    case R.id.Drawable_myAuctions:
+                        Intent in3 = new Intent(getApplicationContext() , HomePage.class);
+                        startActivity(in3);
+                        break;
+                    default:
+                        Intent in6 = new Intent(getApplicationContext() , MyAuctions.class);
+                        startActivity(in6);
+                }
+                return true;
+            }
+        });
+
     }
 }
