36d601bb737317aa1245b6b4b431a83c365fa27e
RaviBx <ravimuay@gmail.com>
2020-09-22 17:26:17 +0530

    Added FDE Helper class

---
+++ app/src/main/java/com/example/online_bidding_system/FdeHelper.java
@@ -0,0 +1,105 @@
+package com.example.online_bidding_system;
+
+public class FdeHelper {
+
+
+        private String Brand,Condition,Material,Size,ContactNo,Description,Duration,Title,Type,Start_Price,Genre;
+
+        public FdeHelper(){
+
+
+        }
+
+
+        public String getBrand() {
+            return Brand;
+        }
+
+        public void setBrand(String brand) {
+            Brand = brand;
+        }
+
+        public String getCondition() {
+            return Condition;
+        }
+
+        public void setCondition(String condition) {
+            Condition = condition;
+        }
+
+        public String getMaterial() {
+            return Material;
+        }
+
+        public void setMaterial(String material) {
+            Material = material;
+        }
+
+        public String getSize() {
+            return Size;
+        }
+
+        public void setSize(String size) {
+            Size = size;
+        }
+
+        public String getContactNo() {
+            return ContactNo;
+        }
+
+        public void setContactNo(String contactNo) {
+            ContactNo = contactNo;
+        }
+
+        public String getDescription() {
+            return Description;
+        }
+
+        public void setDescription(String description) {
+            Description = description;
+        }
+
+        public String getDuration() {
+            return Duration;
+        }
+
+        public void setDuration(String duration) {
+            Duration = duration;
+        }
+
+        public String getTitle() {
+            return Title;
+        }
+
+        public void setTitle(String title) {
+            Title = title;
+        }
+
+        public String getType() {
+            return Type;
+        }
+
+        public void setType(String type) {
+            Type = type;
+        }
+
+        public String getStart_Price() {
+            return Start_Price;
+        }
+
+        public void setStart_Price(String start_Price) {
+            Start_Price = start_Price;
+        }
+
+        public String getGenre() {
+            return Genre;
+        }
+
+        public void setGenre(String genre) {
+            Genre = genre;
+        }
+
+
+    }
+
+
