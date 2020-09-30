4242c7bec241dec9a9134f0b71e9be2724f44748
RaviBx <ravimuay@gmail.com>
2020-09-30 12:06:27 +0530

    Updated Fashion Category page

--- app/src/main/java/com/example/online_bidding_system/Fashion_category.java
+++ app/src/main/java/com/example/online_bidding_system/Fashion_category.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,9 +16,13 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -40,6 +45,23 @@ public class Fashion_category extends AppCompatActivity {
     FdeHelper fCat;
     long maxid=0;
     String idPrefix="FASHION";
+    private String userId;
+
+    private ImageSwitcher imageIs;
+    private  ArrayList<Uri> imageUris;
+    private static final int PICK_IMAGES_CODE = 1;
+    int position = 0;
+
+    private Button preBtn,nxBtn, pickImgbtn;
+    private DatabaseReference mFirebaseDatabase;
+    private DatabaseReference mFirebaseDatabase1;
+    private FirebaseDatabase mFirebaseInstance;
+
+    //Timepicker object
+    TimePicker tp;
+    //Datapicker object
+    DatePicker dp;
+    Adverticement adverticement;
 
 
 
@@ -50,17 +72,21 @@ public class Fashion_category extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fashion_category);
 
-        
+
         Brand        =      findViewById(R.id.fashionBrand);
         Condition    =      findViewById(R.id.fashionCondition);
-        Material     =      findViewById(R.id.fashionMaterial);
+        Material     =      findViewById(R.id.setMaterials);
         Size         =      findViewById(R.id.fashionSize);
-        ContactNo    =      findViewById(R.id.fashionContact);
-        Description  =      findViewById(R.id.fashionDescription);
-        Duration     =      findViewById(R.id.fashionDuration);
-        Title        =      findViewById(R.id.fashionTitle);
+        ContactNo    =      findViewById(R.id.setContact);
+        Description  =      findViewById(R.id.setDescription);
+        Title        =      findViewById(R.id.setTitle);
         Type         =      findViewById(R.id.fashionType);
-        Start_Price  =      findViewById(R.id.fashionPrize);
+        Start_Price  =      findViewById(R.id.setPrice);
+
+        //ged datapicker value
+        dp = findViewById(R.id.setDate);
+        //get Timepicker value
+        tp = findViewById(R.id.setTime);
 
         publishNow   =      findViewById(R.id.publish_now);
 
@@ -68,13 +94,25 @@ public class Fashion_category extends AppCompatActivity {
 
 
         fCat = new FdeHelper();
+        adverticement=new  Adverticement();
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("Adverticement");
+        mFirebaseDatabase1 = mFirebaseInstance.getReference("FashionAndDesign");
+
 
         publishNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                fAuth = FirebaseDatabase.getInstance().getReference().child("FashionDesign");
-                //fAuth = FirebaseDatabase.getInstance().getReference().child("Adverticement");
+                fAuth = FirebaseDatabase.getInstance().getReference().child("FashionAndDesign");
+                fAuth = FirebaseDatabase.getInstance().getReference().child("Adverticement");
+
+                userId = mFirebaseDatabase1.push().getKey();
+                mFirebaseDatabase.child(userId).setValue(adverticement);
+
+                mFirebaseDatabase1.child(userId).setValue(fCat);
+
 
                 fAuth.addValueEventListener(new ValueEventListener() {
                     @Override
@@ -88,6 +126,7 @@ public class Fashion_category extends AppCompatActivity {
 
                     }
                 });
+
 
 
                 try {
@@ -130,16 +169,24 @@ public class Fashion_category extends AppCompatActivity {
                         fCat.setSize(Size.getText().toString().trim());
                         fCat.setContactNo(ContactNo.getText().toString().trim());
                         fCat.setDescription(Description.getText().toString().trim());
-                        fCat.setDuration(Duration.getText().toString().trim());
                         fCat.setTitle(Title.getText().toString().trim());
-                        fCat.setTitle(Type.getText().toString().trim());
                         fCat.setStart_Price(Start_Price.getText().toString().trim());
 
-                        //fAuth.child("fashion1").setValue(fCat);
+                        //set timepicker value
+                        String strTime = tp.getHour() + ":" + tp.getMinute();
+                        adverticement.setDuration(strTime);
+                        //set datapicker value
+                        String strDate =  dp.getYear() + "-" + (dp.getMonth() + 1) + "-" + dp.getDayOfMonth();
+                        adverticement.setDate(strDate);
+
+                        adverticement.setMaxBid("0");
+                        adverticement.setStatus("inactive");
+                        adverticement.setType("FahionAndDesign");
+
 
                         String strNumber= idPrefix+String.valueOf(maxid+1);
                         fAuth.child(String.valueOf(strNumber)).setValue(fCat);
-
+                        fAuth.child(String.valueOf(strNumber)).setValue(adverticement);
 
                         Toast.makeText(getApplicationContext() , "Data Inserted Successfully" , Toast.LENGTH_SHORT).show();
                         clearControl();
@@ -164,7 +211,6 @@ public class Fashion_category extends AppCompatActivity {
                 Description.setText("");
                 Duration.setText("");
                 Title.setText("");
-                Type.setText("");
                 Start_Price.setText("");
             }
 
@@ -174,88 +220,106 @@ public class Fashion_category extends AppCompatActivity {
         });
 
 
-    }
 
-    public void  GetdisplayIntent() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setType("image/*");
-        startActivityForResult(intent, REQUEST_EXTERNAL_STORAGE);
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_EXTERNAL_STORAGE: {
 
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        imageIs = findViewById(R.id.imageIs);
+        preBtn = findViewById(R.id.preButton);
+        nxBtn =  findViewById(R.id.nextButton);
+        pickImgbtn = findViewById(R.id.pickImg);
+        imageUris = new ArrayList<>();
 
-                    GetdisplayIntent();
-                } else {
 
-                }
-                return;
+        imageIs.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView((getApplicationContext()));
+                return imageView;
             }
+        });
 
-        }
+        pickImgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                pickImagesIntent();
+
+            }
+        });
+
+
+
+        preBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(position>0){
+                    position--;
+                    imageIs.setImageURI(imageUris.get(position));
+                }
+
+                else{
+                    Toast.makeText(Fashion_category.this,"Empty",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        nxBtn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(position<imageUris.size()-1){
+
+                    position++;
+                    imageIs.setImageURI(imageUris.get(position));
+                }
+
+                else{
+
+                    Toast.makeText(Fashion_category.this,"empty",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }));
+    }
+
+    private void pickImagesIntent(){
+
+
+
+        Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
+        startActivityForResult(intent, PICK_IMAGES_CODE);
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_EXTERNAL_STORAGE && resultCode == RESULT_OK) {
+        if(requestCode == PICK_IMAGES_CODE){
 
-            final ImageView imageView = findViewById(R.id.image_view);
-            final List<Bitmap> bitmaps = new ArrayList<>();
-            ClipData clipData = data.getClipData();
+            if(resultCode == Activity.RESULT_OK){
+                if(data.getClipData() != null){
 
-            if (clipData != null) {
-                for (int i = 0; i < clipData.getItemCount(); i++) {
-                    Uri imageUri = clipData.getItemAt(i).getUri();
-                    Log.d("URI", imageUri.toString());
-                    try {
-                        InputStream inputStream = getContentResolver().openInputStream(imageUri);
-                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                        bitmaps.add(bitmap);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
+                    int cout  = data.getClipData().getItemCount();
+                    for(int i=0; i<cout; i++){
+                        Uri imageUri = data.getClipData().getItemAt(i).getUri();
+                        imageUris.add(imageUri);
                     }
-                }
-            } else {
 
-                Uri imageUri = data.getData();
-                Log.d("URI", imageUri.toString());
-                try {
-                    InputStream inputStream = getContentResolver().openInputStream(imageUri);
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                    bitmaps.add(bitmap);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                    imageIs.setImageURI(imageUris.get(0));
+                    position = 0;
                 }
 
+                else{
+                    Uri imageUri = data.getData();
+                    imageUris.add(imageUri);
+                    imageIs.setImageURI(imageUris.get(0));
+                    position = 0;
+                }
             }
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for (final Bitmap b : bitmaps) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                imageView.setImageBitmap(b);
-                            }
-                        });
-
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }).start();
         }
+
+
     }
 
 }
