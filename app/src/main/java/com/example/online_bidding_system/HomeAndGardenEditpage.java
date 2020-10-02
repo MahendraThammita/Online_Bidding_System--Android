de5a4e53bead7a339b2697dc1b7b80873cb93171
kaveeshaile <kaveeshaile@gmail.com>
2020-10-02 18:13:54 +0530

    update garden,other categories pages

--- app/src/main/java/com/example/online_bidding_system/HomeAndGardenEditpage.java
+++ app/src/main/java/com/example/online_bidding_system/HomeAndGardenEditpage.java
@@ -3,6 +3,7 @@ package com.example.online_bidding_system;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -24,7 +25,7 @@ import java.util.ArrayList;
 public class HomeAndGardenEditpage extends AppCompatActivity {
     EditText txtTitle,txtPrice,txtContact,txtDescription,txtEnvironment,time,date;
 
-    Button PublishNow,Delete;
+    Button PublishNow,Delete,update;
     DatabaseReference DbRef1,homeref;
 
     Adverticement adverticement;
@@ -47,6 +48,7 @@ public class HomeAndGardenEditpage extends AppCompatActivity {
         txtTitle = findViewById(R.id.setTitle);
         txtPrice = findViewById(R.id.setPrice);
         Delete = findViewById(R.id.Delete);
+        update = findViewById(R.id.Update);
         date=findViewById(R.id.setDate);
         time=findViewById(R.id.setTime);
         txtContact = findViewById(R.id.setContact);
@@ -54,8 +56,11 @@ public class HomeAndGardenEditpage extends AppCompatActivity {
         txtEnvironment= findViewById(R.id.setEnvironment);
 
         adverticement = new Adverticement();
-        DbRef1 = FirebaseDatabase.getInstance().getReference().child("Adverticement").child("HomeAndGarden").child("HAG3");
-        homeref= FirebaseDatabase.getInstance().getReference().child("HomeAndGarden").child("HAG3");
+        Intent retriveIntent = getIntent();
+        final String AuctName = retriveIntent.getStringExtra("AUCT_ID").toString();
+
+        DbRef1 = FirebaseDatabase.getInstance().getReference().child("Adverticement").child(AuctName);
+        homeref= FirebaseDatabase.getInstance().getReference().child("HomeAndGarden").child(AuctName);
         DbRef1.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -93,11 +98,30 @@ public class HomeAndGardenEditpage extends AppCompatActivity {
         Delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DbRef1 = FirebaseDatabase.getInstance().getReference().child("Adverticement").child("HAG3");
+                DbRef1 = FirebaseDatabase.getInstance().getReference().child("Adverticement").child(AuctName);
                 DbRef1.removeValue();
                 Toast.makeText(getApplicationContext() , "Succesfully Deleated" , Toast.LENGTH_SHORT).show();
             }
         });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent retriveIntent = getIntent();
+                String AuctName = retriveIntent.getStringExtra("AUCT_ID").toString();
+
+                DbRef1 = FirebaseDatabase.getInstance().getReference();
+                DbRef1.child("Adverticement").child(AuctName).child("title").setValue(txtTitle.getText().toString().trim());
+                DbRef1.child("Adverticement").child(AuctName).child("contact").setValue(txtContact.getText().toString().trim());
+                DbRef1.child("Adverticement").child(AuctName).child("price").setValue(txtPrice.getText().toString().trim());
+                DbRef1.child("Adverticement").child(AuctName).child("description").setValue(txtDescription.getText().toString().trim());
+                DbRef1.child("Adverticement").child(AuctName).child("date").setValue(date.getText().toString().trim());
+                DbRef1.child("Adverticement").child(AuctName).child("duration").setValue(time.getText().toString().trim());
+                Toast.makeText(getApplicationContext() , "Successfully Updated" , Toast.LENGTH_SHORT).show();
+                Intent displayIntent = new Intent(getApplicationContext(), TabedAuctions.class);
+                startActivity(displayIntent);
+
+            }
+        });
         }
 }
